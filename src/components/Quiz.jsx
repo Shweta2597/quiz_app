63083c22183ec63ffd0112f59ae83c65192409d7
shweta255178 <shwetamds333@gmail.com>
2023-11-17 14:32:00 +0530

    Shuffling answers and adding quiz complete logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImage from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex===QUESTIONS.length
 
   const onAnswerHandler = (selectedAnswer) => {
     setUserAnswers((prevAnswers) => {
@@ -11,12 +14,24 @@ export default function Quiz() {
     });
   };
 
+  if(quizIsComplete){
+    return (
+        <div id="summary">
+            <img src={quizCompleteImage} alt="Trophy image"></img>
+            <h2>Quiz Completed!</h2>
+        </div>
+    )
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(()=>Math.random()-0.5)
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button
                 onClick={() => {
